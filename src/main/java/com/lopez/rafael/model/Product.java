100c726f3d232be7f1b9904620d0196732f1c376
Rafael Lopez <rafael.lopez@bbd.ca>
2021-03-15 22:17:37 -0400

    Add default products via data.sql

--- src/main/java/com/lopez/rafael/model/Product.java
+++ src/main/java/com/lopez/rafael/model/Product.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 @Entity
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into product(name, price, description) values ('Cheese', 3, 'Goat cheese');
+insert into product(name, price, description) values ('Onion', 1, 'Jumbo yellow onion');
+insert into product(name, price, description) values ('Tomato', .8, 'Organic cherry tomato');
+insert into product(name, price, description) values ('Chips', 2.5, 'Chips with BBQ sauce and vinager');
+insert into product(name, price, description) values ('Beans', 1.5, 'Black beans');
+insert into product(name, price, description) values ('Milk', 2.8, 'Nutrient-rich milk');
