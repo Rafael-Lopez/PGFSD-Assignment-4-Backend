255b04d9af89e8882d845e829d48e13b91550860
Rafael Lopez <rafael.lopez@bbd.ca>
2021-03-15 22:06:29 -0400

    Add Product entity & repository + H2 in-memory DB support

--- src/main/java/com/lopez/rafael/application/KitchenStoryApplication.java
+++ src/main/java/com/lopez/rafael/application/KitchenStoryApplication.java
@@ -2,14 +2,18 @@ package com.lopez.rafael.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScans({
 		@ComponentScan("com.lopez.rafael.config"),
 		@ComponentScan("com.lopez.rafael.controller"),
 })
+@EntityScan("com.lopez.rafael.model")
+@EnableJpaRepositories("com.lopez.rafael.repository")
 public class KitchenStoryApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lopez/rafael/config/SecurityConfiguration.java
+++ src/main/java/com/lopez/rafael/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.lopez.rafael.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -10,5 +11,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+            .antMatchers("/h2-console/**");
     }
 }
